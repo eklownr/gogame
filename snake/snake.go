877473bafb2e8e19a6383f11b58d1ddffb17a296
eklownr <eklownr@gmail.com>
2024-12-26 11:52:40 +0100

    move snake

--- snake/snake.go
+++ snake/snake.go
@@ -1,34 +1,77 @@
 package main
 
 import (
+	"image/color"
 	"log"
 
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/vector"
 )
 
 const (
 	screenWidth  = 640
 	screenHeight = 480
+	gridSize     = 20
 )
 
+type Point struct {
+	x, y int
+}
+
 type Game struct {
-	snake []int
+	snake     []Point
+	direction Point
 }
 
 func (g *Game) Update() error {
+	g.updateSnake(&g.snake, g.direction)
 	return nil
 }
 
-func (g *Game) Draw(screen *ebiten.Image) {
+// Update the memory of the snake (*snake) not the copy snake
+func (g *Game) updateSnake(snake *[]Point, dir Point) {
+	head := (*snake)[0]
+	newHead := Point{
+		x: head.x + dir.x,
+		y: head.y + dir.y,
+	}
+	// update the snake. Head + body-1
+	*snake = append([]Point{newHead}, (*snake)[:len(*snake)-1]...)
+}
 
+func (g *Game) Draw(screen *ebiten.Image) {
+	for _, p := range g.snake {
+		vector.DrawFilledRect(
+			screen,
+			float32(p.x*gridSize),
+			float32(p.y*gridSize),
+			gridSize,
+			gridSize,
+			color.White,
+			true,
+		)
+	}
 }
 
 func (g *Game) Layout(outsidewith, outsideheight int) (int, int) {
-	return screenWidth * 2, screenHeight * 2
+	return screenWidth, screenHeight
 }
 
 func main() {
-	if err := ebiten.RunGame(&Game{}); err != nil {
+	ebiten.SetWindowSize(screenWidth, screenHeight)
+	ebiten.SetWindowTitle("Go Snake")
+
+	// initial the snake in to the game
+	g := &Game{
+		snake: []Point{
+			{
+				x: screenWidth / gridSize / 2,
+				y: screenHeight / gridSize / 2,
+			}},
+		direction: Point{x: -1, y: 0},
+	}
+
+	if err := ebiten.RunGame(g); err != nil {
 		log.Fatal(err)
 	}
 }
