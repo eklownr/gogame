10e8b55d8e0e8c15c9e6f347a606078a801ba10d
eklownr <eklownr@gmail.com>
2024-12-26 10:26:05 +0100

    setup basic func

--- snake/snake.go
+++ snake/snake.go
@@ -1,5 +1,34 @@
 package main
 
+import (
+	"log"
+
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+const (
+	screenWidth  = 640
+	screenHeight = 480
+)
+
+type Game struct {
+	snake []int
+}
+
+func (g *Game) Update() error {
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+
+}
+
+func (g *Game) Layout(outsidewith, outsideheight int) (int, int) {
+	return screenWidth * 2, screenHeight * 2
+}
+
 func main() {
-	println("Hello, World!")
+	if err := ebiten.RunGame(&Game{}); err != nil {
+		log.Fatal(err)
+	}
 }
